aebf4a528449554adc557a3c8ecb762864e5968b
davis-matthew <mdavis438@gatech.edu>
2023-08-29 15:38:46 +0000

    Update project src with new namespaces

--- test.sh
+++ test.sh
@@ -1,10 +1,2 @@
-./bin/svf-project  test_cases/test1.bc > out.txt
-if [ -z $(grep "Reachable" out.txt) ]; then
-    echo "test1 error"
-fi
-
-./bin/svf-project  test_cases/test2.bc > out.txt
-if [ -z $(grep "Reachable" out.txt) ]; then
-    echo "test2 error"
-fi
+./bin/svf-project $1 > out.txt
 

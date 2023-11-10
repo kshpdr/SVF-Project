dcaad80c7d0109b94c91d0dae789748c79d09642
root <truemacintoshchannel@gmail.com>
2023-11-10 20:33:07 +0000

    Add function name extraction from call nodes

--- src/project.cpp
+++ src/project.cpp
@@ -23,6 +23,7 @@
 #include "SVF-LLVM/LLVMUtil.h"
 #include "SVF-LLVM/SVFIRBuilder.h"
 #include "WPA/Andersen.h"
+#include "Graphs/PTACallGraph.h"
 
 #include <iostream>
 #include <vector>
@@ -48,7 +49,7 @@ std::vector<std::string> splitString(std::stringstream str, char delim){
 /*!
  * An example to query/collect all successor nodes from a ICFGNode (iNode) along control-flow graph (ICFG)
  */
-void traverseOnICFG(ICFG* icfg, const SVFInstruction* svfinst)
+void traverseOnICFG(PTACallGraph* callgraph, ICFG* icfg, const SVFInstruction* svfinst)
 {
     ICFGNode* iNode = icfg->getICFGNode(svfinst);
     FIFOWorkList<const ICFGNode*> worklist;
@@ -59,6 +60,17 @@ void traverseOnICFG(ICFG* icfg, const SVFInstruction* svfinst)
     while (!worklist.empty())
     {
         const ICFGNode* iNode = worklist.pop();
+
+        if (CallICFGNode::classof(iNode)) {
+            cout << "It's a Call node" << endl;
+            const SVF::CallICFGNode* callNodeConst = dyn_cast<const SVF::CallICFGNode>(iNode);
+            PTACallGraphNode* caller = callgraph->getCallGraphNode(callNodeConst->getCaller());
+            const SVF::SVFFunction* svfFunction = caller->getFunction();
+            cout << svfFunction->getName() << endl;
+        } else {
+            cout << "It's not a Call node" << endl;
+        }
+
         for (ICFGNode::const_iterator it = iNode->OutEdgeBegin(), eit =
                     iNode->OutEdgeEnd(); it != eit; ++it)
         {
@@ -91,30 +103,24 @@ int main(int argc, char ** argv) {
 
     /// This is necessary if we want to consider indirect function calls. It can be interesting to speak with a TA
     /// To see if this is necessary. This can also plot the call graph which is more intuitive than the ICFG.
-    // PTACallGraph* callgraph = AndersenWaveDiff::createAndersenWaveDiff(svfir)->getPTACallGraph();
-    // builder.updateCallGraph(callgraph);
-    // callgraph->dump("/home/project/graphs/call_graph_" + graphFileName);
+    PTACallGraph* callgraph = AndersenWaveDiff::createAndersenWaveDiff(svfir)->getPTACallGraph();
+    builder.updateCallGraph(callgraph);
+    callgraph->dump("/home/project/graphs/call_graph_" + graphFileName);
 
     /// ICFG
     ICFG* icfg = svfir->getICFG();
     // icfg->updateCallGraph(callgraph); // This is necessary when considering indirect function calls.
     icfg->dump("/home/project/graphs/icfg_" + graphFileName);
 
-
-    Andersen* ander = AndersenWaveDiff::createAndersenWaveDiff(svfir);
-    PTACallGraph* callgraph = ander->getPTACallGraph();
-
-    VFG* vfg = new VFG(callgraph);
-
-    /// Sparse value-flow graph (SVFG)
-    SVFGBuilder svfBuilder;
-    SVFG* svfg = svfBuilder.buildFullSVFG(ander);
-    LLVMModuleSet::getLLVMModuleSet()->dumpModulesToFile(".svf.bc");
-
     const SVFFunction* mainFunc = svfModule->getSVFFunction("main");
-    // const BasicBlock* entryBB = &(mainFunc->getLLVMFuntcion()->getEntryBlock());
     const SVFInstruction* mainInst = mainFunc->getEntryBlock()->front();
-    traverseOnICFG(icfg, mainInst);
+
+    // Example of getting a function name of the call node
+    SVF::PTACallGraphNode* ptaCallNode = callgraph->getCallGraphNode(mainFunc);
+    const SVF::SVFFunction* svfFunction = ptaCallNode->getFunction();
+    cout << svfFunction->getName() << endl;
+
+    traverseOnICFG(callgraph, icfg, mainInst);
 
     return 0;
 }
